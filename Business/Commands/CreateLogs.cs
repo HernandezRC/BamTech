7e745097de4f47e62b18fd3fe71d4431cdef4b68
HernandezRC <ryanchernandez@gmail.com>
2025-01-10 08:59:58 -0500

    Attempt at starting Log, did a undo on other attempts to actually execute the logs. Wont have enough time before meeting to try and get it working.

---
+++ Business/Commands/CreateLogs.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using StargateAPI.Business.Data;
+using StargateAPI.Controllers;
+
+namespace StargateAPI.Business.Commands
+{
+    public class CreateLogs : IRequest<CreateLogsResult>
+    {
+        public required Logs Logs { get; set; }
+    }
+
+    public class CreateLogsHandler(StargateContext context) : IRequestHandler<CreateLogs, CreateLogsResult>
+    {
+        private readonly StargateContext _context = context;
+
+        public async Task<CreateLogsResult> Handle(CreateLogs request, CancellationToken cancellationToken)
+        {
+            var newLog = new Logs()
+            {
+                LogDateTime = request.Logs.LogDateTime,
+                LogMessage = request.Logs.LogMessage,
+                LogException = request.Logs.LogException,
+                LogLevel = request.Logs.LogLevel
+            };
+
+            await _context.Logs.AddAsync(newLog, cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new CreateLogsResult()
+            {
+                Id = newLog.Id
+            };
+        }
+    }
+
+    public class CreateLogsResult : BaseResponse
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Business/Data/Logs.cs
@@ -0,0 +1,11 @@
+﻿namespace StargateAPI.Business.Data
+{
+    public class Logs
+    {
+        public int Id {  get; set; }
+        public DateTime LogDateTime { get; set; }
+        public string LogLevel { get; set; } = string.Empty;
+        public string LogMessage { get; set; } = string.Empty;
+        public string LogException { get; set; } = string.Empty;
+    }
+}
